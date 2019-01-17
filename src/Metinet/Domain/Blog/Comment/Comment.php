d186b87743c66acc42ab4e859adfaceaaf932620
Boris Guéry <guery.b@gmail.com>
2019-01-17 09:05:46 +0100

    feat(comment): Comment cannot be posted with bad words

--- src/Metinet/Domain/Blog/Comment/Comment.php
+++ src/Metinet/Domain/Blog/Comment/Comment.php
@@ -7,6 +7,8 @@ class Comment
     private $body;
     private $author;
 
+    public const BAD_WORDS = ['fuck', 'whore', 'shit', 'scum'];
+
     public static function post(string $body, string $author): Comment
     {
         return new self($body, $author);
@@ -22,12 +24,29 @@ class Comment
         return $this->author;
     }
 
+    private function ensureNoBadWords(string $body)
+    {
+        $badWordsFoundCount = count(
+            array_intersect(
+                array_map('strtolower', self::BAD_WORDS),
+                explode(' ', preg_replace("/[^A-Za-z0-9' -]/",'', strtolower($body)))
+            )
+        );
+
+        if ($badWordsFoundCount > 0) {
+
+            throw new BadWordsDetected();
+        }
+    }
+
     private function __construct(string $body, string $author)
     {
         if (empty($body)) {
 
             throw UnableToPostComment::cannotPostWithAnEmptyBody();
         }
+
+        $this->ensureNoBadWords($body);
 
         $this->body = $body;
         $this->author = $author;
--- tests/Metinet/Domain/Blog/Comment/CommentTest.php
+++ tests/Metinet/Domain/Blog/Comment/CommentTest.php
@@ -2,6 +2,7 @@
 
 namespace Metinet\Tests\Metinet\Domain\Blog\Comment;
 
+use Metinet\Domain\Blog\Comment\BadWordsDetected;
 use Metinet\Domain\Blog\Comment\UnableToPostComment;
 use PHPUnit\Framework\TestCase;
 use Metinet\Domain\Blog\Comment\Comment;
@@ -36,7 +37,12 @@ class CommentTest extends TestCase
 
     public function testACommentCannotBePostedWithBadWords(): void
     {
+        $this->expectException(BadWordsDetected::class);
+        $this->expectExceptionMessage('Bad words have been detected in your comment');
 
+        $bodyWithBadWords = 'You are such a scumbag full of shit';
+        $author = 'Pline';
+        Comment::post($bodyWithBadWords, $author);
     }
 
     public function testADeletionReasonMustBeProvidedWhenDeletingAComment(): void
@@ -47,6 +53,7 @@ class CommentTest extends TestCase
     public function testACommentCannotHaveAnEmptyBody(): void
     {
         $this->expectException(UnableToPostComment::class);
+
         $this->expectExceptionMessage('Cannot post a Comment with an empty body');
         $emptyBody = '';
         $author = 'Pline';
