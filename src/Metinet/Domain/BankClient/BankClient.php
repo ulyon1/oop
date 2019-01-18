53b71e75c419ca7d9c26a2de2ede0a02a07cba9d
Yoann Dufour <yoann01.d@gmail.com>
2019-01-18 09:21:19 +0100

    Creation de l'architecture + instantiation des tests

---
+++ src/Metinet/Domain/BankClient/BankClient.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lp
+ * Date: 18/01/2019
+ * Time: 09:20
+ */
+
+namespace Metinet\Domain\BankClient;
+
+
+class BankClient
+{
+
+}
---
+++ tests/Metinet/Domain/BankClient/BankClientTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lp
+ * Date: 18/01/2019
+ * Time: 09:08
+ */
+
+namespace Metinet\Domain\BankClient;
+
+use PHPUnit\Framework\TestCase;
+
+class BankClientTest extends TestCase
+{
+    public function testABankClientCanBeCreated()
+    {
+
+    }
+
+    public function testABankClientCanDoADeposit()
+    {
+
+    }
+
+    public function testABankClientCanDoAWithdraw()
+    {
+
+    }
+
+    public function testABankClientCanTakeALookOnHisHistory(){
+
+    }
+
+}
