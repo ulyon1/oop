c8416374769dc2e11a3f7984a61795f2c0d043b2
ElliotLEdwards <elliot.edwards38@gmail.com>
2019-01-18 11:37:47 +0100

    projet banque Elliot Edwards

--- tests/Metinet/SampleTest.php
+++ tests/Metinet/SampleTest.php
@@ -3,11 +3,29 @@
 namespace Metinet;
 
 use PHPUnit\Framework\TestCase;
+use Metinet\Domain\Bank\Account;
+use Metinet\Domain\Bank\Client;
+use Metinet\Domain\Bank\Operation;
 
 class SampleTest extends TestCase
 {
     public function testEnsurePhpUnitIsWorkingProperly(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function testCreateBankAccount(): void
+    {
+        $account = new Account(new Money(500, new Currency('EUR')), new Money(0, new Currency('EUR')));
+    }
+
+    public function makeDepositToBankAccount(): void
+    {
+        
+    }
+
+    public function makeNegativeValueDeposit(): void
+    {
+        
     }
 }
